ffd314c9274b36de254a8a6b800e9b7084e70b9c
Lashonda Miller <lashondamiller007@gmail.com>
2021-10-11 10:48:33 -0400

    Add project files.

---
+++ SalesWebAPI/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebAPI.Models
+{
+    public class AppDbContext : DbContext {
+
+        //DbSet collections go here
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
